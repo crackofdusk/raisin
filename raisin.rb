d6b9a3eaabb04a41ab9686208d8ac9117c064345
Dimiter Petrov <crackofdusk@gmail.com>
2016-08-05 09:13:54 +0200

    Implement assert_equal

--- raisin.rb
+++ raisin.rb
@@ -4,18 +4,36 @@ end
 class NothingRaised < StandardError
 end
 
+def test_failure(&block)
+  raised = false
+  begin
+    yield
+  rescue AssertionError
+    raised = true
+  end
+  raise NothingRaised unless raised
+end
+
 def assert(condition)
   raise AssertionError unless condition
 end
 
 assert(true)
 
-raised = false
-begin
+test_failure do
   assert(false)
-rescue AssertionError
-  raised = true
 end
-raise NothingRaised unless raised
+
+
+def assert_equal(expected, actual)
+  assert(expected == actual)
+end
+
+assert_equal(true, true)
+
+test_failure do
+  assert_equal("foo", "bar")
+end
+
 
 puts "Success!"
